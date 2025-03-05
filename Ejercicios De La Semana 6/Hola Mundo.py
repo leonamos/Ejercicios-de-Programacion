a195cab62a81afc5c5e476e3724cef64828d2068
leonamos <lm0609032025@unab.edu.sv>
2025-03-04 21:28:25 -0600

    commit

---
+++ Ejercicios De La Semana 6/Hola Mundo.py
@@ -0,0 +1,11 @@
+print("Hola Mundo")
+
+# Comentario
+'''Comentario de varias lineas'''
+
+mensaje = "Hola Mundo"
+mensaje = "Bienvenido a Python"
+print(mensaje)
+# Cambiamos el valor de la variable mensaje
+mensaje = "Bienvenidos a Python 3"
+print(mensaje)
---
+++ Ejercicios De La Semana 6/Numeros en Python.py
@@ -0,0 +1,8 @@
+# Tipos de datos numericos 
+numero = 10
+numero2 = "10"
+numero3 = 12.645
+total = numero + int(numero2)
+print(type(total))
+print(type(numero2))
+print(type(numero3))
