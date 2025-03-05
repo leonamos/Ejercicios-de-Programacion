b2289411ffdcd13e09e1fc57dea85c03fe3e0ea6
leonamos <lm0609032025@unab.edu.sv>
2025-03-04 23:07:37 -0600

    Ejercicios de la Semana 6

--- Ejercicios De La Semana 6/logica.py
+++ Ejercicios De La Semana 6/logica.py
@@ -1,6 +1,6 @@
 #un cajero automatico
-usuario = list()
 
+usuario = list()
 usuario = list("amosleon") # string str
 usuario.append("1234")      # string str
 usuario.append(1000)        # int
@@ -13,25 +13,20 @@ recibo.append(["126", 500])
 recibo.append(["127", 100])
 recibo.append(["128", 1000])
 
-
-
-
 acciones = list()
 acciones.append("depositar")
 acciones.append("pago de recibo")
 acciones.append("retirar")
 
-
+usuario2 = list()
 usuario2 = list("leon")     # string str
 usuario2.append("12345")    # string str
 usuario2.append(1500)       # int
 
 def registrar():
-    UsuarioRegistrado = list()
     usuarioC = input("Ingrese su nombre: ")
-    UsuarioRegistrado.append("Ingrese su password: ")
-
-    if "usuarioC" == usuario[0] and UsuarioRegistrado[0] == usuario[1]:
+    usuarioRegistrado = input("Ingrese su password: ")
+    if usuario == usuarioC[0] and usuarioRegistrado[0] == usuario[1]:
         print("Bienvenido al cajero automatico")
     else:
         print("Usuario o contraseña incorrectos")
